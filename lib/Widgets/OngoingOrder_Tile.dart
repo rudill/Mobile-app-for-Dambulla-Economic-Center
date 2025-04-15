f443f38150e79e75fd331641a2ef4c6f1f05d238
Dillon Williams <dillonwilliams200161@gmail.com>
2025-04-15 21:31:54 +0530

    Create OngoingOrder Page

---
+++ lib/Widgets/OngoingOrder_Tile.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class OnOrderTile extends StatefulWidget {
+  const OnOrderTile({super.key});
+
+  @override
+  State<OnOrderTile> createState() => _OnOrderTileState();
+}
+
+class _OnOrderTileState extends State<OnOrderTile> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
