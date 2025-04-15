23575b66a8c390eb1b47f96a9ed0e6786c79fe3c
Dillon Williams <dillonwilliams200161@gmail.com>
2025-04-16 00:59:52 +0530

    add Style for ongoingOrder Card

--- lib/Widgets/OngoingOrder_Tile.dart
+++ lib/Widgets/OngoingOrder_Tile.dart
@@ -10,49 +10,79 @@ class OnOrderTile extends StatefulWidget {
 class _OnOrderTileState extends State<OnOrderTile> {
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return Padding(
+      padding: const EdgeInsets.all(6.0),
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
             children: [
-              Text("කැරට්  200Kg"),
-              Container( decoration: BoxDecoration(
-                border: Border.all(color: Colors.black),
-                borderRadius: BorderRadius.circular(5)
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("කැරට්  200Kg",style: TextStyle( fontWeight: FontWeight.w700, fontSize: 15),),
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+                    decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black),
+                    borderRadius: BorderRadius.circular(5)
+                  ),
+                    child: Text("1KG රු. 200.00",style: TextStyle( fontWeight: FontWeight.w500, fontSize: 15),),
+                  ),
+                ],
               ),
-                child: Text("කිලෝවකට රු. 200.00"),
+              SizedBox(height: 10,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      SizedBox(height: 55,width: 55,
+                        child: CircularProgressIndicator(
+                          value: 0.2,
+                          color: Color(0xFF208A43),
+                          backgroundColor: Color(0xD1D1D3E4),
+                          strokeWidth: 5,
+                        ),
+                      ),
+                      Text(
+                        "20%",
+                        style: TextStyle(
+                          fontSize: 10,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ],
+                  ),
+
+                  Text("200Kg න් 40Kg සම්පුර්ණ වී ඇත",style: TextStyle( fontWeight: FontWeight.w500, fontSize: 15),)
+                ],
               ),
+              SizedBox(height: 10,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(onPressed: (){},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.green,
+                        foregroundColor: Colors.white,
+                      ),
+                      child: Text("වෙනස් කරන්න",style: TextStyle( fontWeight: FontWeight.w700)),),
+
+                  ElevatedButton(onPressed: (){},
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.amber,
+                        foregroundColor: Colors.white,
+                      ),
+                      child: Text("ඉවත් කරන්න",style: TextStyle( fontWeight: FontWeight.w700)),),
+
+                ],
+              )
             ],
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              CircularProgressIndicator(),
-              Text("200Kg න් 40Kg සම්පුර්ණ වී ඇත")
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              ElevatedButton(onPressed: (){},
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.green,
-                    foregroundColor: Colors.white,
-                  ),
-                  child: Text("වෙනස් කරන්න")),
-
-              ElevatedButton(onPressed: (){},
-                  style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.amber,
-                    foregroundColor: Colors.white,
-                  ),
-                  child: Text("ඉවත් කරන්න")),
-
-
-            ],
-          )
-        ],
+        ),
       ),
     );
   }
