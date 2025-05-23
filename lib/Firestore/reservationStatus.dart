138d527790c634eda9ea313d2992b2404f28b9be
Dillon Williams <dillonwilliams200161@gmail.com>
2025-05-24 02:00:55 +0530

    Create oderHistory in farmer side

--- lib/Firestore/reservationStatus.dart
+++ lib/Firestore/reservationStatus.dart
@@ -4,9 +4,8 @@ import 'package:intl/intl.dart';
 class OrderWaitingData {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
-  Stream<List<Map<String, dynamic>>> getOrderWaitingData(
-    String farmerID,
-  ) async* {
+  Stream<List<Map<String, dynamic>>> getOrderWaitingData(String farmerID,)
+  async* {
     try {
       final orderSnapshot =
           await _firestore
@@ -58,4 +57,58 @@ class OrderWaitingData {
       yield [];
     }
   }
+
+  Stream<List<Map<String, dynamic>>> getPastOrders(String farmerID) async*{
+    try{
+      final orderSnapshot =
+      await _firestore
+          .collection('orderHistory')
+          .where('farmerID', isEqualTo: farmerID)
+          .where('status', isEqualTo: 'completed')
+          .get();
+
+      List<Map<String, dynamic>> combinedData = [];
+
+      for (var orderHistoryDoc in orderSnapshot.docs) {
+        var orderHistoryData = orderHistoryDoc.data();
+
+        var sellerDoc =
+        await _firestore
+            .collection('SellerReg')
+            .doc(orderHistoryData['sellerID'])
+            .get();
+        var sellerData = sellerDoc.data();
+
+        var productDoc =
+        await _firestore
+            .collection('Product')
+            .doc(orderHistoryData['productID'])
+            .get();
+        var productData = productDoc.data();
+
+        String formattedDate = '';
+        if (orderHistoryData['date'] is Timestamp) {
+          DateTime dateTime = (orderHistoryData['date'] as Timestamp).toDate();
+          formattedDate = DateFormat('yyyy-MM-dd').format(dateTime);
+        }
+
+        if (sellerDoc.exists) {
+          combinedData.add({
+            'shopName': sellerData?['ShopName'] ?? '',
+            'productName': productData?['name'],
+            'quantity': orderHistoryData['quantity'],
+            'date': formattedDate,
+            'totalPrice': orderHistoryData['totalPrice'],
+          });
+        }
+      }
+
+      yield combinedData;
+
+    }catch(e){
+      print('Error$e');
+    }
+
+  }
+
 }
